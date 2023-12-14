4662306217681e657688f864f5d00b483be9ac0d
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-12-14 11:19:52 +0100

    edited default schema for masterpiece blog

--- schemas/post.js
+++ schemas/post.js
@@ -20,12 +20,6 @@ export default defineType({
       },
     }),
     defineField({
-      name: 'author',
-      title: 'Author',
-      type: 'reference',
-      to: {type: 'author'},
-    }),
-    defineField({
       name: 'mainImage',
       title: 'Main image',
       type: 'image',
@@ -54,7 +48,6 @@ export default defineType({
   preview: {
     select: {
       title: 'title',
-      author: 'author.name',
       media: 'mainImage',
     },
     prepare(selection) {
